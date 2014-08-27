c343c05044ee8274d91af60d0044d8abda45771d
foxandrewd <andrewfox@Andrews-MacBook-Pro.local>
2014-08-27 15:33:04 +1000

    Now keeping only the 'keep' CPGs from Naeem paper

--- filter.R
+++ filter.R
@@ -1,1 +1,21 @@
+
+setwd("~/Documents/methyl/epi_data")
+
+fil <- read.csv("Naeem_Haroon_filter_450kchip.csv", head=T)
+
 keep <- fil[fil$Flag.discard.keep.=="keep",]
+load("Mvals_betaThreshold0_001.RData")
+load("Mlincheck.RData")
+
+## We now have the main datasets
+
+MvKeep = Mv[ as.character(keep$probe) , ]
+MlinKeep = Mlin[ as.character(keep$probe) , ]
+
+wasKept_Mv = rownames(MvKeep)        # 
+wasKept_Mlin = rownames(MlinKeep)    # 
+
+### save(MvKeep, file="Mvals_Filtered_betaThreshold0_001.RData")
+### save(MlinKeep, file="Mlincheck_Filtered.RData")
+
+
